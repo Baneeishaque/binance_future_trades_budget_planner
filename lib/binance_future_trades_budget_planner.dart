512ea4e1cc8078e639e464fd4dc24a52a481dcef
Banee Ishaque K <Baneeishaque@gmail.com>
2023-07-03 14:04:41 +0530

    In

---
+++ lib/binance_future_trades_budget_planner.dart
@@ -0,0 +1,10 @@
+/// binance_future_trades_budget_planner, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate binance_future_trades_budget_planner
+/// dart pub global activate binance_future_trades_budget_planner
+///
+/// # see usage
+/// binance_future_trades_budget_planner --help
+/// ```
+library binance_future_trades_budget_planner;
